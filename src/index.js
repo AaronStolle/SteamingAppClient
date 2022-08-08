45b9aa7a0c3481795156787ebd978094b33603b3
Aaron Stolle <aaronstolle@gmail.com>
2022-08-08 10:50:09 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './components/App.js';
+import reducers from './reducers'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const composeEnhancers = window.__REDUX_DEVTOOLS_ENTENSION_COMPOSE__ || compose
+
+// here we are using composeWithDevTools from npm package to use the devtool
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.querySelector('#root'))
